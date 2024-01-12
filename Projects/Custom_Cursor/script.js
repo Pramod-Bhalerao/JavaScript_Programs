0df833b4ceff3429c3a19a097948cce59e23eee4
Pramod-Bhalerao <146328095+Pramod-Bhalerao@users.noreply.github.com>
2024-01-12 12:18:00 +0530

    Delete Projects directory

--- Projects/Custom_Cursor/script.js
+++
@@ -1,7 +0,0 @@
-let main = document.querySelector("#main")
-let crsr = document.querySelector(".cursor")
-
-main.addEventListener("mousemove", function(stb){
-    crsr.style.left = stb.x + "px"
-    crsr.style.top = stb.y + "px"
-})
